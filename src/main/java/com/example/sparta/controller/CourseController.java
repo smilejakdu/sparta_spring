19ba8b16a5b52882346fc1dfc4f51dae4fa28de0
smilejakdu <ash982416@gmail.com>
2023-02-11 13:26:52 +0900

    modify course post

---
+++ src/main/java/com/example/sparta/controller/CourseController.java
@@ -0,0 +1,47 @@
+package com.example.sparta.controller;
+
+import com.example.sparta.domain.Course;
+import com.example.sparta.domain.CourseRepository;
+import com.example.sparta.domain.CourseRequestDto;
+import com.example.sparta.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CourseController {
+    private final CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    @GetMapping("/courses")
+    public List<Course> getCourses() {
+        return courseRepository.findAll();
+    }
+
+    // PostMapping을 통해서, 같은 주소라도 방식이 다름을 구분합니다.
+    @PostMapping("/api/courses")
+    public Course createCourse(@RequestBody CourseRequestDto requestDto) {
+        // requestDto 는, 생성 요청을 의미합니다.
+        // 강의 정보를 만들기 위해서는 강의 제목과 튜터 이름이 필요하잖아요?
+        // 그 정보를 가져오는 녀석입니다.
+
+        // 저장하는 것은 Dto가 아니라 Course이니, Dto의 정보를 course에 담아야 합니다.
+        // 잠시 뒤 새로운 생성자를 만듭니다.
+        Course course = new Course();
+        course.setTitle(requestDto.getTitle());
+        course.setTutor(requestDto.getTutor());
+
+        // JPA를 이용하여 DB에 저장하고, 그 결과를 반환합니다.
+        return courseRepository.save(course);
+    }
+
+    @PutMapping("/api/courses/{id}")
+    public Long updateCourse(
+            @PathVariable Long id,
+            @RequestBody CourseRequestDto requestDto
+    ) {
+        return courseService.update(id, requestDto);
+    }
+}
